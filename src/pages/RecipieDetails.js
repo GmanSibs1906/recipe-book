2b4c5deb2ea195bc2662a2295d5e011620b84487
Bongz Podolski <ntshingilab@outlook.com>
2023-09-30 18:33:36 +0200

    finally got it to render the product by id

--- src/pages/RecipieDetails.js
+++ src/pages/RecipieDetails.js
@@ -7,10 +7,12 @@ function RecipieDetails() {
     const params = useParams();
     return (
         <div className="recipieDetails">
-            <p>This is the id : {params.id}</p>
             <div className="recipieList">
-                {recipeDataList.map((recepie) =>
-                    <RecipieDetailsCard key={recepie.id} id={recepie.id} title={recepie.title} image={recepie.image} recipe={recepie.recipe} />
+                {recipeDataList.map((recepie) => {
+                    if (recepie.id == params.id) {
+                        return <RecipieDetailsCard key={recepie.id} id={recepie.id} title={recepie.title} image={recepie.image} recipe={recepie.recipe} />
+                    }
+                }
                 )}
             </div>
         </div>
